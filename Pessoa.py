61d6e763335292d398137767820efa8927f97888
Roginie Almeida dos Santos <48979948883@info.local>
2024-08-21 19:12:38 -0300

    cadastro

--- Pessoa.py
+++ Pessoa.py
@@ -1,52 +1,63 @@
 from datetime import date
 
-#CLASSE PESSOA ENDERECO
 class Endereco:
-  def __init__(self, logradouro='', numero='', endereco=None, endereco_comecial=False):
-    self.lagradouro = logradouro
-    self.numero = numero
-    self.endereco_comecial = endereco_comecial
+    def __init__(self, logradouro="", numero="", enderecoComercial=False):
+        self.logradouro = logradouro
+        self.numero = numero
+        self.enderecoComercial = enderecoComercial
 
-#CLASSE PESSOA
+
 class Pessoa:
-  def __init__(self, nome='', rendimento=0.0, endereco=None):
-    self.nome = nome
-    self.rendimento = rendimento
-    self.endereco = endereco
+    def __init__(self, nome="", rendimento=0.0, endereco=None):
+        self.nome = nome
+        self.rendimento = rendimento
+        self.endereco = endereco
 
-  def calcula_imposto(self, rendimento):
-    return rendimento
-#CLASSE PESSOA FISICA
+    def calcularImposto(self, rendimento):
+        return 
+
 class PessoaFisica(Pessoa):
-  def __init__(self, cpf='', nome='', rendimento=0.0, endereco=None):
-    super().__init__(nome, rendimento=0.0, endereco=None, cpf='', dataNacimento=None)
-    if endereco is None:
-      endereco = Endereco()
+    def __init__(self, nome="", rendimento=0.0, endereco=None, cpf="", dataNascimento=""):
+        if endereco is None:
+            endereco = Endereco()
 
-    if dataNacimento is None:
-      dataNacimento = date.today()
+        if dataNascimento is None:
+            dataNascimento = date.today()
+        
+        super().__init__(nome, rendimento, endereco)
 
-    super().__init__(nome, rendimento, endereco)
-    #chama o construtor da superclass pessoa para inicializar os atribrutos herdado
+        self.cpf = cpf
+        self.dataNascimento = dataNascimento
 
+    def calcularImposto(self, rendimento: float) -> float:
+        if rendimento <= 10500:
+            return 0
+        elif 10500 < rendimento <= 30500:
+            return (rendimento / 100) * 5
+        elif 30500 < rendimento <= 60000:
+            return (rendimento / 100) * 8
+        else:
+            return (rendimento / 100) * 12
 
-
-
-    #Atributos da propria classe
-    self.cpf = cpf
-    self.dataNacimento = dataNacimento
-
-  def calcula_imposto(self, rendimento: float) -> float:
-      if rendimento <= 1500: #sem imposto para redimento
-        return 0
-      elif 1500 < rendimento <= 3500:
-        return rendimento * 0.2
-      elif 3500 < rendimento <= 6000:
-        return (rendimento / 100) * 3.5
-      else:
-        return rendimento * 5
-#CLASSE PESSOA JURIDICA
 class PessoaJuridica(Pessoa):
-  pass
+    def __init__(self, nome="", rendimento=0.0, endereco=None, cnpj="", dataNascimento=""):
+        if endereco is None:
+            endereco = Endereco()
 
+        if dataNascimento is None:
+            dataNascimento = date.today()
 
+        super().__init__(nome, rendimento, endereco)
+
+        self.cnpj = cnpj
+        self.dataNascimento = dataNascimento
+
+        def calcularImposto(self, rendimento: float) -> float:
+            if rendimento <= 1500:
+                return 0
+            elif 1500 < rendimento <= 3500:
+                return (rendimento / 100) * 2
+            elif 3500 < rendimento <= 6000:
+                return (rendimento / 100) * 3.5
+            else:
+                return (rendimento / 100) * 5
--- sistema.py
+++ sistema.py
@@ -1,72 +1,197 @@
-#1 - pessoa fisica / 2 - pessoa juridica /3 - sair
-#1 - castrar pessoa fisica / 2 - listar pessoa fisica / 3 sair
 from datetime import date, datetime
-from Pessoa import PessoaFisica
-from Pessoa import Endereco, PessoaFisica
+from Pessoa import Endereco, PessoaFisica, PessoaJuridica
+
 
 def main():
-  lista_pf = []
-  while True:
-    opcao = input('1 - pessoa fisica / 2 - pessoa juridica /3 - sair ')
-    if opcao == '1':
-      while True:
-        opcao = input('1 - castrar pessoa fisica / 2 - listar pessoa fisica / 3 sair ')
-        if opcao == '1':
-          novopf = PessoaFisica()
-          novo_end_pf = Endereco()
 
-          novopf.nome = input('Digite o nome da pessoa fisica: ')
-          novopf.cpf = input('Digite o cpf da pessoa fisica: ')
-          novopf.rendimento = float(input('Digite o rendimento da pessoa fisica: '))
+    listaPF = []
+    listaJ = []
+    while True:
+        opcao = int(input("Escolha uma opcao: 1 - Pessoa Fisica / 2 - Pessoa Juridica / 0 - Sair: "))
+        if opcao == 1:
+            while True:
+                opcaoPF = int(input("Escolha uma opcao: 1 - Cadastrar Pessoa Fisica / 2 - Listar Pessoa Fisica / 3 - Remover Pessoa Fisica / 4 - Atualizar Pessoa Fisica / 0 - Voltar menu anterior: "))
+                if opcaoPF == 1:
+                    novaPF = PessoaFisica()
+                    novoEnderecoPF = Endereco()
 
-          data_nacimento = input('Digite a data de nascimento da pessoa fisica: ')
-          novopf.dataNacimento = datetime.strptime(data_nacimento, '%d/%m/%Y').date()
-          idade = (date.today() - novopf.dataNacimento).days // 365
-          if idade < 18:
-            print('Pessoa fisica menor de idade')
-          else:
-            print('Pessoa fisica maior de idade')
-            continue
-          
-          novo_end_pf.lagradouro = input('Digite o logradouro da pessoa fisica: ')
-          novo_end_pf.numero = input('Digite o numero da pessoa fisica: ')
-          end_comercial = input('A pessoa fisica possui endereco comercial? (S/N) ')
-          novo_end_pf.endereco_comecial = end_comercial.strip().upper() == 'S' 
+                    novaPF.nome = input("Digite o nome da Pessoa Fisica: ")
+                    novaPF.cpf = input("Digite o cpf da Pessoa Fisica: ")
+                    novaPF.rendimento = float(input("Digite o rendimento mensal da Pessoa Fisica(SOMENTE NUMEROS): "))
+                    
+                    data_Nascimento = input("Digite a data de nascimento (dd/mm/aaaa): ")
+                    novaPF.dataNascimento = datetime.strptime(data_Nascimento, '%d/%m/%Y').date()
+                    idade = (date.today() - novaPF.dataNascimento).days // 365
 
-          novopf.endereco = novo_end_pf
+                    if idade >= 18:
+                        print("Pessoa Fisica tem mais de 18 anos")
+                    else:
+                        print("Pessoa Fisica tem menos de 18 anos. Retorne ao menu...")
+                        continue
 
-          lista_pf.append(novopf)
-          print('Pessoa fisica cadastrada com sucesso!')
-          continue
+                    novoEnderecoPF.logradouro = input("Digite o Logradouro: ")
+                    novoEnderecoPF.numero = input("Digite o numero: ")
+
+                    endereco_Comercial = input("Esse endereco e comercial? (S/N): ")
+                    novoEnderecoPF.enderecoComercial = endereco_Comercial.strip().upper() == "S"
+
+                    novaPF.endereco = novoEnderecoPF
+
+                    listaPF.append(novaPF)
+
+                    print("Cadastro realizado com sucesso!")
+
+                elif opcaoPF == 2:
+                    if listaPF:
+                        for cadaPF in listaPF:
+                            print()
+                            print(f"Nome: {cadaPF.nome}")
+                            print(f"CPF: {cadaPF.cpf}")
+                            print(f"Endereco: {cadaPF.endereco.logradouro}, N: 1{cadaPF.endereco.numero}")
+                            print(f"Data Nascimento: {cadaPF.dataNascimento.strftime('%d/%m/%Y')}")
+                            print(f"Imposto a ser pago: {cadaPF.calcularImposto(cadaPF.rendimento)}")
+                            print("Digite 0 para continuar")
+                            input()
+                    else:
+                        print("Lista vazia!")
+
+                elif opcaoPF == 3:
+                    removerCPF = input("Digite o cpf da Pessoa Fisica que sera excluido: ")
+                    pessoaEncontrada = False
+
+                    for cadaPF in listaPF:
+                        if cadaPF.cpf == removerCPF:
+                            listaPF.remove(cadaPF)
+                            pessoaEncontrada = True
+                            print("Pessoa fisica removida")
+                            break
+
+                    if not pessoaEncontrada:       
+                        print("Nenhuma pessoa encontrada")
+
+                elif opcaoPF == 4:
+                    print("Atualizar o os items")
+                    pessoaEncontrada = False
+
+                    atualizarCadastroPF = int(input("Digite qual valor voce quer alterar: 1 - Nome / 2 - Endereco / 3 - Data de Nascimento / 4 - Imposto"))
+
+                    if atualizarCadastroPF  == 1:
+                        novaPF.nome = input("Digite o nome da Pessoa Fisica: ")
+                    
+                    elif atualizarCadastroPF  == 2:
+                        novoEnderecoPF = Endereco()
+                    
+                    elif atualizarCadastroPF  == 3:
+                        data_Nascimento = input("Digite a data de nascimento (dd/mm/aaaa): ")
+                        novaPF.dataNascimento = datetime.strptime(data_Nascimento, '%d/%m/%Y').date()
+                        idade = (date.today() - novaPF.dataNascimento).days // 365
+                    
+                    elif atualizarCadastroPF == 4:
+                        novaPF.rendimento = float(input("Digite o rendimento mensal da Pessoa Fisica(SOMENTE NUMEROS): "))
+
+                elif opcaoPF == 0:
+                    print("Voltando ao menu anterior")
+                    break
+
+                else:
+                    print("Opcao invalida, por favor digite uma das opcoes indicadas: ")
+
         elif opcao == 2:
-            if lista_pf:
-             for cada_pf in lista_pf:
-              print(f'Nome: {cada_pf.nome}')
-              print(f'CPF: {cada_pf.cpf}')
-              print(f'endereço: {cada_pf.endereco.lagradouro}, N{cada_pf.endereco}')
-              print(f'Data de nascimento: {cada_pf.dataNacimento.strftime('%d/%m/%y')}')
-              print(f'Imposto: {cada_pf.calcula_imposto(cada_pf.rendimento)}')
-              print('Digite 0 para sair')
-              input()
-            else:
-                print('Nenhuma pessoa fisica cadastrada')
-          
-        elif opcao == '3':
-            print('Saindo...')
+            print("Funcionalidade para pessoa juridica nao implementada")
+            pass
+
+        elif opcao == 0:
+            print("Obrigado por utilizar o nosso sistema!")
             break
         else:
-            print('Opção invalida')
+            print("Opcao invalida! Por favor digite uma das opcoes validas!")
 
-    elif opcao == 2:
-      print('Fucionalidade para pessoa juridica nao implementas')
-      pass
+        if opcao == 2:
+            while True:
+                opcaoJ = int(input("Escolha uma opcao: 1 - Cadastrar Pessoa Juridica / 2 - Listar Pessoa Juridica / 3 - Excluir Pessoa Juridica / 0 - Para voltar o menu "))
+                if opcaoJ == 1:
+                    novaJ = PessoaJuridica()
+                    novoEnderecoJ = Endereco()
 
-    elif opcao == 0:
-      print('obrigado por utilizar o nosso sistema valeu')
-      break
-    else:
-      print('opacao invalida por favor digita uma das opcao validas')
+                    novaJ.nome = input("Digite o nome da Pessoa Juridica: ")
+                    novaJ.cnpj = input("Digite o cnpj da Pessoa Juridica: ")
+                    novaJ.rendimento = float(input("Digite o rendimento mensal da Pessoa Juridica(SOMENTE NUMEROS): "))
+                    
+                    data_Nascimento = input("Digite a data de nascimento (dd/mm/aaaa): ")
+                    novaJ.dataNascimento = datetime.strptime(data_Nascimento, '%d/%m/%Y').date()
+                    idade = (date.today() - novaJ.dataNascimento).days // 365
 
-if __name__=='__main__':
-  main()#chama a funcao principal
-  
+                    if idade >= 18:
+                        print("Pessoa Juridica tem mais de 18 anos")
+                    else:
+                        print("Pessoa Juridica tem menos de 18 anos. Retorne ao menu...")
+                        continue
+
+                    novoEnderecoJ.logradouro = input("Digite o Logradouro: ")
+                    novoEnderecoJ.numero = input("Digite o numero: ")
+
+                    endereco_Comercial = input("Esse endereco e comercial? (S/N): ")
+                    novoEnderecoJ.enderecoComercial = endereco_Comercial.strip().upper() == "S"
+
+                    novaJ.endereco = novoEnderecoJ
+
+                    listaJ.append(novaJ)
+
+                    print("Cadastro realizado com sucesso!")
+
+                elif opcaoJ == 2:
+                    if listaJ:
+                        for cadaJ in listaJ:
+                            print(f"Nome: {cadaJ.nome}")
+                            print(f"CPF: {cadaJ.cnpj}")
+                            print(f"Endereco: {cadaJ.endereco.logradouro}, N: 1{cadaJ.endereco.numero}")
+                            print(f"Data Nascimento: {cadaJ.dataNascimento.strftime('%d/%m/%Y')}")
+                            print(f"Imposto a ser pago: {cadaJ.calcularImposto(cadaJ.rendimento)}")
+                            print("Digite 0 para sair")
+                            input()
+                    else:
+                        print("Lista vazia!")
+
+                elif opcaoJ == 3:
+                    removerCNPJ = input("Digite o cnpj da Pessoa Juridica que sera excluido: ")
+                    pessoaEncontrada = False
+
+                    for cadaJ in listaJ:
+                        if cadaJ.cnpj == removerCNPJ:
+                            listaJ.remove(cadaJ)
+                            pessoaEncontrada = True
+                            print("Pessoa Juridica removida")
+                            break
+
+                    if not pessoaEncontrada:       
+                        print("Nenhuma pessoa encontrada")
+
+                elif opcaoPF == 4:
+                    print("Atualizar o os items")
+                    pessoaEncontrada = False
+
+                    atualizarCadastroPJ = int(input("Digite qual valor voce quer alterar: 1 - Nome / 2 - Endereco / 3 - Data de Nascimento / 4 - Imposto"))
+
+                    if atualizarCadastroPJ  == 1:
+                        novaJ.nome = input("Digite o nome da Pessoa Juridica: ")
+                    
+                    elif atualizarCadastroPJ  == 2:
+                        novoEnderecoJ = Endereco()
+                    
+                    elif atualizarCadastroPJ  == 3:
+                        data_Nascimento = input("Digite a data de nascimento (dd/mm/aaaa): ")
+                        novaJ.dataNascimento = datetime.strptime(data_Nascimento, '%d/%m/%Y').date()
+                        idade = (date.today() - novaPF.dataNascimento).days // 365
+                    
+                    elif atualizarCadastroPJ == 4:
+                        novaJ.rendimento = float(input("Digite o rendimento mensal da Pessoa Juridica(SOMENTE NUMEROS): "))
+
+                elif opcaoJ == 0:
+                    print("Voltando ao menu anterior")
+                    break
+
+                else:
+                    print("Opcao invalida, por favor digite uma das opcoes indicadas: ")
+
+if __name__ == "__main__":
+    main()
